c94d170e3678c107721cc437289ae44034a9eb6b
Chris Chen <christinafh.chen@gmail.com>
2018-06-06 00:26:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(x)
   if x%3 ==0 && x%5==0 
-    puts "fizzbuzz"
+    "fizzbuzz"
     elsif x%3==0 
-    puts "fizz"
+    "fizz"
   elsif x%5==0 
-    puts "buzz"
+    "buzz"
   else
     puts "nil"
   end 
