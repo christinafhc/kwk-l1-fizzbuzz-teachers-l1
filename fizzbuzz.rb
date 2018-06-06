02f20929d81c2510d8de97eee430ca30d9fbbb9f
Chris Chen <christinafh.chen@gmail.com>
2018-06-06 00:27:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,19 @@
-def fizzbuzz(x)
-  if x%3 ==0 && x%5==0 
-    "fizzbuzz"
-    elsif x%3==0 
-    "fizz"
-  else x%5==0 
-    "buzz"
-  end
-end 
+# def fizzbuzz(x)
+#   if x%3 ==0 && x%5==0 
+#     "fizzbuzz"
+#     elsif x%3==0 
+#     "fizz"
+#   else x%5==0 
+#     "buzz"
+#   end
+# end 
 
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
+  end
